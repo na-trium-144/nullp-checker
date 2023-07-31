16bab680c59782aecd02b74da43228a7d454b84f
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-08-01 00:38:42 +0900

    arrayがネストしても使えるようになった

--- npchk.hpp
+++ npchk.hpp
@@ -1,6 +1,8 @@
+#pragma once
 #include <memory>
 #include <string>
 #include <stdexcept>
+#include <type_traits>
 
 namespace npchk {
 struct NpChkBase {
@@ -30,6 +32,13 @@ struct NpChkBase {
     void failNullPtr() const {
         throw std::runtime_error((name + " is nullptr").c_str());
     }
+    void failOutOfRange(std::size_t n, std::size_t size) const {
+        throw std::out_of_range(
+            (name + "[" + std::to_string(n) +
+             "] is out of range (size = " + std::to_string(size) + ")")
+                .c_str());
+    }
+    virtual void updateName(){}
 };
 template <typename T>
 class shared_ptr : public NpChkBase {
@@ -47,6 +56,7 @@ class shared_ptr : public NpChkBase {
     template <typename U>
     auto operator=(const U &rhs) {
         base = rhs;
+        return *this;
     }
     operator Base() const {
         check();
@@ -59,6 +69,51 @@ class shared_ptr : public NpChkBase {
     auto operator*() const { return *get(); }
     auto operator->() const { return get(); }
     // auto operator[](std::ptrdiff_t i) const { return get()[i]; }
+};
+template <typename T, std::size_t N>
+class array : public NpChkBase, public std::array<T, N> {
+    using Base = std::array<T, N>;
+    Base &base() { return *this; }
+    const Base &base() const { return *this; }
+    void check(std::size_t n) const {
+        if (n >= N) {
+            failOutOfRange(n, N);
+        }
+    }
+
+  public:
+    array() = default;
+    explicit array(const char *names) : NpChkBase(names), Base() {
+        updateName();
+    }
+    void updateName() override{
+        if constexpr(std::is_base_of_v<NpChkBase, T>){
+            for (std::size_t n = 0; n < N; ++n) {
+                base()[n].name = this->name + "[" + std::to_string(n) + "]";
+                base()[n].updateName();
+            }
+        }
+    }
+    template <typename U>
+    auto operator=(const U &rhs) {
+        base() = rhs;
+        return *this;
+    }
+    auto &at(std::size_t n) {
+        check(n);
+        return base()[n];
+    }
+    const auto &at(std::size_t n) const {
+        check(n);
+        return base()[n];
+    }
+    auto &operator[](std::size_t n) { return at(n); }
+    const auto &operator[](std::size_t n) const { return at(n); }
+    auto &front() { return at(0); }
+    const auto &front() const { return at(0); }
+    auto &back() { return at(N ? N - 1 : 0); }
+    const auto &back() const { return at(N ? N - 1 : 0); }
+    // 書きかけ
 };
 } // namespace npchk
 
@@ -73,7 +128,15 @@ class shared_ptr : public NpChkBase {
     v1(A), v2(A), v3(A), v4(A), v5(A), v6(A), v7(A)
 #define NPCHK8(A, v1, v2, v3, v4, v5, v6, v7, v8)                              \
     v1(A), v2(A), v3(A), v4(A), v5(A), v6(A), v7(A), v8(A)
-#define GETNAME8(_1, _2, _3, _4, _5, _6, _7, _8, NAME, ...) NAME
+#define NPCHK_GETNAME8(_1, _2, _3, _4, _5, _6, _7, _8, NAME, ...) NAME
+// https://stackoverflow.com/questions/24481810/how-to-remove-the-enclosing-parentheses-with-macro
+#define NPCHK_DEPAREN(X) NPCHK_ESC(NPCHK_V2 X)
+#define NPCHK_V2(...) NPCHK_V2 __VA_ARGS__
+#define NPCHK_ESC(...) NPCHK_ESC2(__VA_ARGS__)
+#define NPCHK_ESC2(...) NPCHK_V_##__VA_ARGS__
+#define NPCHK_V_NPCHK_V2
 #define NPCHK(type, ...)                                                       \
-    type GETNAME8(__VA_ARGS__, NPCHK8, NPCHK7, NPCHK6, NPCHK5, NPCHK4, NPCHK3, \
-                  NPCHK2, NPCHK1)(#__VA_ARGS__, __VA_ARGS__)
+    NPCHK_DEPAREN(type)                                                        \
+    NPCHK_GETNAME8(__VA_ARGS__, NPCHK8, NPCHK7, NPCHK6, NPCHK5, NPCHK4,        \
+                   NPCHK3, NPCHK2, NPCHK1, )                                   \
+    (#__VA_ARGS__, __VA_ARGS__)
